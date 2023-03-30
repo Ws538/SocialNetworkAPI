362cfc7cb8213e4507ac3d8b2109ece69a29bf3c
Ws538 <williamsu668@gmail.com>
2023-03-30 15:57:36 +1100

    completed models, added api route files

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+const { User, Thought} = require('../models');
+
+module.exports = {
+  getUsers(req, res) {
+    User.find()
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+}
